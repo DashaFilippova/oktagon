a568b862e010bc62d218732b6ddbbcb0cd64e6d3
DashaFilippova <dasha.filippova2004@mail.ru>
2024-01-26 14:58:24 +0800

    задание 2

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const checkParams = ["a", "b", "c"];
+
 app.get('/', (req, res) => {
   res.send('<h1>Привет, Октагон!</h1>');
 });
@@ -11,17 +13,22 @@ app.get('/static', (req, res) => {
 });
 
 app.get('/dynamic', (req, res) => {
+  // Проверяем наличие всех параметров и их тип
+  for (let param of checkParams) {
+    if (!req.query.hasOwnProperty(param) || isNaN(parseFloat(req.query[param]))) {
+      res.json({ header: 'Error' });
+      return;
+    }
+  }
+
   const a = parseFloat(req.query.a);
   const b = parseFloat(req.query.b);
   const c = parseFloat(req.query.c);
 
-  if (isNaN(a) ||isNaN(b)| isNaN(c)) {
-    res.json({ header: 'Error' });
-  } else {
-    const result = (a * b * c) / 3;
-    res.json({ header: 'Calculated', body: result.toString() });
-  }
+  const result = (a * b * c) / 3;
+  res.json({ header: 'Calculated', body: result.toString() });
 });
+
 app.listen(port, () => {
     console.log(`Сервер запущен по адресу http://localhost:${port}`
     );
